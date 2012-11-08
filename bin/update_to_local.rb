c886ad133c2ce6805df53b74a7952f69a0546c5b
Francis Hwang <sera@fhwang.net>
2012-11-08 08:06:31 -0500

    Vim stuff

--- bin/update_to_local.rb
+++ bin/update_to_local.rb
@@ -1,5 +1,3 @@
-#!/usr/bin/env ruby
-
 require File.dirname(__FILE__) + "/../lib/dot-files"
 DotFiles.targets.each do |target|
   `cp -r ./files/#{target} ~/.#{target}`
--- lib/dot-files.rb
+++ lib/dot-files.rb
@@ -1,5 +1,5 @@
 module DotFiles
   def self.targets
-    %w(bash_profile)
+    %w(bash_profile vimrc.after gvimrc.after)
   end
 end
