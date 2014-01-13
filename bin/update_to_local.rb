93b05812c81878757da19b6cad87193863b2f565
Francis Hwang <sera@fhwang.net>
2014-01-13 15:19:10 -0500

    Add autocomplete for migreazy.

--- bin/update_to_local.rb
+++ bin/update_to_local.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + "/../lib/dot-files"
+require File.expand_path(File.dirname(__FILE__)) + "/../lib/dot-files"
 DotFiles.targets.each do |target|
   `cp -r ./files/#{target} ~/.#{target}`
 end
